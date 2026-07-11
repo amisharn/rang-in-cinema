67153c81c6bf13fb855c1df1dac075ea0784d820
amisharn <niroulaamisharaj@gmail.com>
2026-07-11 19:14:28 +0545

    Implemented Histogram Comparison and Visualization

--- backend/app/image_utils.py
+++ backend/app/image_utils.py
@@ -1,11 +1,17 @@
 import cv2 as cv
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
-def load_image(path):
+
+def process_image(path):
     img = cv.imread(path)
     if img is None:
-        sys.exit("Image couldn't be read!")
-    return img
+        sys.exit("Can't read Image!")
+    hsv = convert_to_hsv(img)
+    hist = extract_histogram(hsv)
+    normalized = normalize_hist(hist)
+    return normalized
 
 def convert_to_hsv(img):
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -23,12 +29,67 @@ def normalize_hist(hist):
     normalized = cv.normalize(hist,None,1,0,cv.NORM_L1)
     return normalized
 
-if __name__ == "__main__":
-    img = load_image('dataset/movies/image.jpeg')
-    hsv = convert_to_hsv(img)
-    hist = extract_histogram(hsv)
-    normalized = normalize_hist(hist)
+#Experimental histogram comparison function p.s. i am not using this for retrieval
+def compare_hist_manual(hist1,hist2):
+    total_diff = 0
+    for x,y in zip(hist1, hist2):
+        if x == 0 and y == 0:
+            continue
+        total_diff += (((x-y)/(x+y))**2)
+    similarity = 1-total_diff
+    return similarity
 
-    print(hsv.shape , normalized)
+#Compared with opencv function using the correlation metric
+def compare_hist_opencv(hist1, hist2):
+    similarity = cv.compareHist(hist1,hist2,cv.HISTCMP_CORREL)
+    return similarity
+
+def plot_histograms(hist1,hist2):
+    labels = ["Red","Yellow","Green","Cyan","Blue","Magenta"]
+
+    x = np.arange(len(labels))
+
+    width = 0.35
+
+    plt.figure(figsize=(8,5))
+
+    plt.bar(x-width/2,hist1,width,label = "Fallen Angels")
+    plt.bar(x+width/2,hist2,width,label = "Chungking Express")
+
+    plt.xticks(x,labels)
+
+    plt.xlabel("Hue Bucket")
+    plt.ylabel ("Normalized Frequency")
+    plt.title ("Hue Distribution")
+
+    plt.legend()
+    plt.show()
+
+
+    
+if __name__ == "__main__":
+    normalized1 = process_image('dataset/movies/fallen_angels.png')
+    normalized2 = process_image('dataset/movies/chungking_express.png')
+
+    similarity_manual = compare_hist_manual(normalized1, normalized2)
+    print(f"Similarity Manual: {similarity_manual:.02f}")
+
+    correlation = compare_hist_opencv(normalized1, normalized2)
+    print(f"Correlation: {correlation:.02f}")
+    if correlation > 0.9:
+        print ("Very Similar")
+    elif correlation > 0.5:
+        print ("Somewhat Similar")
+    elif correlation >0:
+        print("Weakly Similar")
+    else:
+        print("Very Different")
+
+    plot_histograms(normalized1,normalized2)
+
+
+
+
+
 
 
--- docs/devlog.md
+++ docs/devlog.md
@@ -3,8 +3,29 @@ Day 1
 What did I do?
 
 - Set up the project structure.
+
 - Created functions to load image, convert to hsv from bgr, extract the histogram and flatten the histogram and normalize the distributiion values.
 
 What did I learn?
 
 - Learned the importance of feature extraction, why hsv is more suitable than bgr for this project, and why should the distribution be normalized. I also learned about Single Responsibility Principle of SE and how should a project be structured.
+
+Day 2
+
+What did I do?
+
+- Manually created a comparison function for histogram for learning purpose
+
+- Used OpenCV compareHist() using correlation metric
+
+- Compared and plotted the histogram to analyze distribution using Matplotlib
+
+What did I learn?
+
+- My manual function was not bounded
+
+- You can use metrics like correlation, intersection, bhattacharya distance and chi-square for comparison
+
+- Correlation is the most suitable for this project as we try to find out how similar the color distributions are in the images
+
+- How to visualize feature vectors using Matplotlib
